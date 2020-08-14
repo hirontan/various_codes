2b24a351a8e2752bc11104972d4c5115de470590
hirontan <hirontan@sora.flights>
2020-08-15 03:17:41 +0900

    to_sをつけなくてもgeohashが取得できる

--- src/ruby/geo.rb
+++ src/ruby/geo.rb
@@ -3,4 +3,4 @@ require 'sora_geocoding'
 puts SoraGeocoding.search('東京都')
 coordinate = SoraGeocoding.coordinates('東京都千代田区')
 latlon = coordinate[:coordinates]
-puts SoraGeocoding.geohash(latlon[:lat].to_s, latlon[:lon].to_s)
+puts SoraGeocoding.geohash(latlon[:lat], latlon[:lon])
