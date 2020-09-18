812d7d57e3fb4675876097ee82601458ee8bc3f6
hirontan <hirontan@sora.flights>
2020-09-19 02:56:29 +0900

    [Python] 名前空間とスコープ

---
+++ src/python/scope.py
@@ -0,0 +1,18 @@
+"""
+Test Test ##############################
+"""
+
+animal = 'cat'
+
+
+def f():
+    # global animal = 'dog'
+    print('local:', locals)
+    print(f.__name__)
+    print(f.__doc__)
+
+
+f()
+print(__name__)
+print('global:', animal)
+print('global:', globals)
