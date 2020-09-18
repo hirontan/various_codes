72c6ceaad2104321a7934c3cbd28a2fb06456fa0
hirontan <hirontan@sora.flights>
2020-09-19 02:43:26 +0900

    [Python] リスト内包表記

--- src/python/flow_control.py
+++ src/python/flow_control.py
@@ -107,3 +107,25 @@ d = {'x': 100, 'y': 200}
 print(d.items())
 for k, v in d.items():  # アンパッキングでkとvに入る
     print(k, ':', v)
+
+# リスト内包表記
+t = (1, 2, 3, 4, 5)
+t2 = (5, 6, 7, 8, 9, 10)
+
+r = []
+for i in t:
+    r.append(i)
+
+print(r)
+
+r = [i for i in t if i & 2 == 0]
+print(r)
+
+r = []
+for i in t:
+    for j in t2:
+        r.append(i + j)
+
+print(r)
+r = [i * j for i in t for j in t2]
+print(r)
