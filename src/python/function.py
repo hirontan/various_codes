7e858d3ba112beadc141c09f97275ef3d25d5467
hirontan <hirontan@sora.flights>
2020-09-19 02:31:36 +0900

    [Python] Lambda

--- src/python/function.py
+++ src/python/function.py
@@ -186,3 +186,25 @@ print(r)
 # r = add_num(10, 20)
 # print('end')
 # print(r)
+
+l = ['Mon', 'tue', 'Wed', 'Thu', 'fri', 'sat', 'Sun']
+
+
+def change_words(words, func):
+    for word in words:
+        print(func(word))
+
+
+def sample_func(word):
+    return word.capitalize()
+
+
+def sample_func2(word):
+    return word.lower()
+
+# sample_func = lambda word: word.capitalize()
+
+
+# change_words(l, sample_func)
+change_words(l, lambda word: word.capitalize())
+change_words(l, lambda word: word.lower())
