dcd74d02ebe288eaba65ab1dfe0606f7fbcf71f0
hirontan <hirontan@sora.flights>
2020-09-19 02:48:49 +0900

    [Python] 集合内包表記

--- src/python/flow_control.py
+++ src/python/flow_control.py
@@ -143,3 +143,15 @@ print(d)
 
 d = {x: y for x, y in zip(w, f)}
 print(d)
+
+# 集合内包表記
+s = set()
+
+for i in range(10):
+    if i % 2 == 0:
+        s.add(i)
+
+print(s)
+
+s = {i for i in range(10) if i % 2 == 0}
+print(s)
