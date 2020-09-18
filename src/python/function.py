53703a86d0e9450991ea9943959c6aa9dc92a95f
hirontan <hirontan@sora.flights>
2020-09-18 21:51:33 +0900

    [Python] Docstrings

--- src/python/function.py
+++ src/python/function.py
@@ -93,3 +93,21 @@ d = {
 }
 
 menu2('test', 'test2', 'test3', **d)
+
+
+def example_func(param1, param2):
+    """Example
+
+    Args:
+      param1 (int): The first parameter
+      param2 (int): The second parameter
+
+    Returns:
+      bool: The return Value.
+    """
+    print(param1)
+    print(param2)
+    return True
+
+
+print(example_func.__doc__)
