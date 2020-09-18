6fd7df8f2a282670240ef0948d6025696fcf8374
hirontan <hirontan@sora.flights>
2020-09-18 22:10:43 +0900

    [Python] デコレータ

--- src/python/function.py
+++ src/python/function.py
@@ -149,3 +149,40 @@ ca2 = circle_area_func(3.141592)
 
 print(ca1(10))
 print(ca2(10))
+
+
+def print_more(func):
+    def wrapper(*args, **kwargs):
+        print('func:', func.__name__)
+        print('args:', args)
+        print('kwargs:', kwargs)
+
+        result = func(*args, **kwargs)
+        print('result:', result)
+        return result
+    return wrapper
+
+
+def print_info(func):
+    def wrapper(*args, **kwargs):
+        print('start')
+
+        result = func(*args, **kwargs)
+        print('end')
+        return result
+    return wrapper
+
+
+@print_info  # デコレータ（前後に処理したい場合に扱う）
+@print_more
+def add_num(a, b):
+    return a + b
+
+
+r = add_num(10, 20)
+print(r)
+
+# print('start')
+# r = add_num(10, 20)
+# print('end')
+# print(r)
