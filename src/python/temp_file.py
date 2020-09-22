e00a8b1b1436869ef021d866588bac02e2d55ab8
hirontan <hirontan@sora.flights>
2020-09-22 23:38:07 +0900

    [Python] tempfileの活用

---
+++ src/python/temp_file.py
@@ -0,0 +1,20 @@
+import tempfile
+
+with tempfile.TemporaryFile(mode='w+') as t:
+    t.write('file')
+    t.seek(0)
+    print(t.read())
+
+with tempfile.NamedTemporaryFile(delete=False) as t:
+    print(t.name)
+    with open(t.name, 'w+') as f:
+        f.write('test\n')
+        f.seek(0)
+        print(f.read())
+
+with tempfile.TemporaryDirectory() as td:
+    print(td)
+
+
+temp_dir = tempfile.mkdtemp()
+print(temp_dir)
