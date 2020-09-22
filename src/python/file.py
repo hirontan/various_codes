477a41a0999746330b3e23f173b99692c327fd7d
hirontan <hirontan@sora.flights>
2020-09-22 21:59:49 +0900

    [Python] ファイルの読み込み

--- src/python/file.py
+++ src/python/file.py
@@ -10,3 +10,18 @@
 # with ステートメントを利用するとcloseを行ってくれる
 with open('test.txt', 'w') as f:
     f.write('test\n')
+
+s = """\
+AAA
+BBB
+CCC
+DDD
+"""
+
+with open("test.txt", 'r') as f:
+    while True:
+        chunk = 2
+        line = f.read(chunk)
+        print(line)
+        if not line:
+            break
