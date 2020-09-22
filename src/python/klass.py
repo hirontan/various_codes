9f9eb2351dcb013d32241acdc9225770dfc5c251
hirontan <hirontan@sora.flights>
2020-09-22 16:36:04 +0900

    [Python] クラスの初期化とクラス変数

--- src/python/klass.py
+++ src/python/klass.py
@@ -1,8 +1,19 @@
 # Python2の名残でobjectと書いておくと方が良い
+from os import name
+
+
 class Person(object):
+    def __init__(self, name):
+        self.name = name
+        print(self.name)
+
     def say_somthing(self):
-        print('hello')
+        print('I am {}.'.format(self.name))
+        self.run(10)
+
+    def run(self, num):
+        print('run' * num)
 
 
-person = Person()
+person = Person('Mike')
 person.say_somthing()
