12dfe962cd5e4563f942367adc32e2ca32c125bf
hirontan <hirontan@sora.flights>
2020-09-22 17:05:18 +0900

    [Python] ダックタイピング

--- src/python/inheritance.py
+++ src/python/inheritance.py
@@ -1,9 +1,47 @@
+class Person(object):
+    def __init__(self, age=1):
+        self.age = age
+
+    def drive(self):
+        if self.age >= 18:
+            print('ok')
+        else:
+            raise Exception('No drive')
+
+
+class Baby(Person):
+    def __init__(self, age=1):
+        if age < 18:
+            super().__init__(age)
+        else:
+            raise ValueError
+
+
+class Adult(Person):
+    def __init__(self, age=18):
+        if age >= 18:
+            super().__init__(age)
+        else:
+            raise ValueError
+
+
+baby = Baby()
+adult = Adult()
+
+
 class Car(object):
     def __init__(self, model=None):
         self.model = model
 
     def run(self):
         print('run')
+
+    def ride(self, person):
+        person.drive()
+
+
+car = Car()
+car.ride(adult)
 
 
 class ToyotaCar(Car):
