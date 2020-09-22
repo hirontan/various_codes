702d6ec0b55552fa14991b0753eb33f61bd4afa6
hirontan <hirontan@sora.flights>
2020-09-22 21:56:42 +0900

    [Python] withステートメント

--- src/python/file.py
+++ src/python/file.py
@@ -1,5 +1,12 @@
 # f = open('test.txt', 'w')
-f = open('test.txt', 'a')
-f.write('test\n')
-print('My', 'name', 'is', 'Mike', sep='#', file=f)
-f.close()
+# f = open('test.txt', 'a')
+# f.write('test\n')
+# print('My', 'name', 'is', 'Mike', sep='#', file=f)
+
+# # メモリを使ってしまう
+# f.close()
+
+
+# with ステートメントを利用するとcloseを行ってくれる
+with open('test.txt', 'w') as f:
+    f.write('test\n')
