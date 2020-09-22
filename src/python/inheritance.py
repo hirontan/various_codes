4b1a742730c97655e583a3063fa239d80aa067c5
hirontan <hirontan@sora.flights>
2020-09-22 16:47:41 +0900

    [Python] メソッドのオーバーライドとsuper

--- src/python/inheritance.py
+++ src/python/inheritance.py
@@ -1,13 +1,25 @@
 class Car(object):
+    def __init__(self, model=None):
+        self.model = model
+
     def run(self):
         print('run')
 
 
 class ToyotaCar(Car):
-    pass
+    def run(self):
+        print('fast')
 
 
 class TeslaCar(Car):
+    def __init__(self, model='Model S', enable_auto_run=False):
+        # self.model = model
+        super().__init__(model)
+        self.enable_auto_run = enable_auto_run
+
+    def run(self):
+        print('fffffast')
+
     def auto_run(self):
         print('auto run')
 
@@ -15,9 +27,11 @@ class TeslaCar(Car):
 car = Car()
 car.run()
 
-toyota_car = ToyotaCar()
+toyota_car = ToyotaCar('Lexes')
+print(toyota_car.model)
 toyota_car.run()
 
-tesla_car = TeslaCar()
+tesla_car = TeslaCar('Model S')
+print(tesla_car.model)
 tesla_car.run()
 tesla_car.auto_run()
