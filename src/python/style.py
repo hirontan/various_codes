9b0b0a91a4f3e6702b3e8bc214f073f0c790a739
hirontan <hirontan@sora.flights>
2020-09-23 09:51:08 +0900

    [Python] スタイルコード

---
+++ src/python/style.py
@@ -0,0 +1,15 @@
+x = 1
+y = 2
+
+x = 'zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+
+
+def test_func(a, b, c):
+    """[summary]
+
+    Args:
+        a ([type]): [description]
+        b ([type]): [description]
+        c ([type]): [description]
+    """
+    pass
--- src/python/sub_process.py
+++ src/python/sub_process.py
@@ -8,6 +8,10 @@ subprocess.run(['ls', '-al'])
 # Exceptiomをあげてくれる
 # r = subprocess.run('ls', shell=True, check=True)
 
+
 p1 = subprocess.Popen(['ls', '-al'], stdout=subprocess.PIPE)
 p2 = subprocess.Popen(['grep', 'test'], stdin=p1.stdout,
                       stdout=subprocess.PIPE)
+p1.stdout.close()
+output = p2.communicate()[0]
+print(output)
