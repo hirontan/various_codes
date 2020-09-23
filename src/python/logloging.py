1150a2139b14a5541489d928c901bd257d736647
hirontan <hirontan@sora.flights>
2020-09-23 10:34:35 +0900

    [Python] logging formatter

--- src/python/logloging.py
+++ src/python/logloging.py
@@ -8,7 +8,9 @@ DEBUG
 
 import logging
 
-logging.basicConfig(filename='test.log', level=logging.INFO)
+# 標準ドキュメントをみると、フォーマットで利用できるものがわかる
+formatter = '%(asctime)s:%(levelname)s:%(message)s'
+logging.basicConfig(filename='test.log', level=logging.INFO, format=formatter)
 
 # logging.info('info {}'.format('test'))
 # logging.info('info %s %s' % ('test', 'test'))
