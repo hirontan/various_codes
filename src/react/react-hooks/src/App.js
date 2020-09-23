7d2e8a4abcf66a67c0d368f3edc68e9eb6904fb6
hirontan <hirontan@sora.flights>
2020-09-24 01:33:55 +0900

    [React] React Hooks / useStateでファンクションコンポーネントに状態を持たせる

--- src/react/react-hooks/src/App.js
+++ src/react/react-hooks/src/App.js
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
-  return <div>This is a template for React app</div>;
+  // 状態, 設定
+  const [count, setCount] = useState(0);
+
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+
+  const increment2 = () => setCount((previousCount) => previousCount + 1);
+  const decrement2 = () => setCount((previousCount) => previousCount - 1);
+
+  const reset = () => setCount(0);
+  const double = () => setCount(count * 2);
+  const divide3 = () =>
+    setCount((previousCount) =>
+      previousCount % 3 === 0 ? previousCount / 3 : previousCount
+    );
+
+  return (
+    <>
+      <div>count: {count}</div>
+      <div>
+        <button onClick={increment}>+1</button>
+        <button onClick={decrement}>-1</button>
+      </div>
+      <div>
+        <button onClick={increment2}>+1</button>
+        <button onClick={decrement2}>-1</button>
+      </div>
+      <div>
+        <button onClick={reset}>Reset</button>
+        <button onClick={double}>x2</button>
+        <button onClick={divide3}>3の倍数の時だけ3で割る</button>
+      </div>
+    </>
+  );
 };
 
 export default App;
