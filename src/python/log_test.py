c1109c6532941ce1033ebe9b85ce8e4a6e6c843e
hirontan <hirontan@sora.flights>
2020-09-23 11:01:29 +0900

    [Python] logging handler

--- src/python/log_test.py
+++ src/python/log_test.py
@@ -4,5 +4,9 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 
+h = logging.FileHandler('logtest.log')
+logger.addHandler(h)
+
+
 def do_something():
     logger.info('from logtest')
