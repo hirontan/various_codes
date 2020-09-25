4b65942d7e11fff7349125cf7c38cd88a41d3553
hirontan <hirontan@sora.flights>
2020-09-26 02:29:44 +0900

    [React] Provider/Consumerを活用（Consumerはコメントアウト）

--- src/react/react-hooks/src/components/App.js
+++ src/react/react-hooks/src/components/App.js
@@ -5,19 +5,24 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import EventForm from "./EventForm";
 
 import Events from "./Events";
+import AppContext from "../contexts/AppContext";
 
 // indexは省略できる
 import reducer from "../reducers";
+
+console.log({ AppContext });
 
 const App = () => {
   // 第三引数には初期化時に入れたい状態を格納できる
   const [state, dispatch] = useReducer(reducer, []);
 
   return (
-    <div className="container-fluid">
-      <EventForm state={state} dispatch={dispatch} />
-      <Events state={state} dispatch={dispatch} />
-    </div>
+    <AppContext.Provider value={"Hello, I am a Provider."}>
+      <div className="container-fluid">
+        <EventForm state={state} dispatch={dispatch} />
+        <Events state={state} dispatch={dispatch} />
+      </div>
+    </AppContext.Provider>
   );
 };
 
--- src/react/react-hooks/src/components/Events.js
+++ src/react/react-hooks/src/components/Events.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import Event from "./Event";
+import AppContext from "../contexts/AppContext";
 
 const Events = ({ state, dispatch }) => {
+  const value = useContext(AppContext);
   return (
     <>
+      {/* <AppContext.Consumer>
+        {(value) => {
+          return <div>{value}</div>;
+        }}
+      </AppContext.Consumer> */}
+      {/* <div>{value}</div> */}
       <h4>イベント一覧</h4>
       <table className="table table-hover">
         <thead>
