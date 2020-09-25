2027597573d8af67aa7f4b19d3993c6a1740bda1
hirontan <hirontan@sora.flights>
2020-09-25 22:45:50 +0900

    [React] イベントの状態遷移を管理するreducerを作成

---
+++ src/react/react-hooks/src/reducers/index.js
@@ -0,0 +1,56 @@
+import { unstable_renderSubtreeIntoContainer } from "react-dom";
+
+// stateが未定義の場合があるので、初期化しておく
+
+// action = {
+//   type: 'CREATE_EVENT',
+//   title: 'テスト',
+//   body: 'テスト'
+// }
+
+// # before
+// state = []
+
+// # after
+// state = [
+//   {
+//     id: 1,
+//     title: 'テスト',
+//     body: 'テスト'
+//   }
+// ]
+
+// # before
+// state = [
+//   {id: 1, title: 'タイトル', body: 'ボディー'},
+//   {id: 2, title: 'タイトル', body: 'ボディー'},
+//   {id: 3, title: 'タイトル', body: 'ボディー'}
+// ]
+
+// # after
+// state = [
+//   {id: 1, title: 'タイトル', body: 'ボディー'},
+//   {id: 2, title: 'タイトル', body: 'ボディー'},
+//   {id: 3, title: 'タイトル', body: 'ボディー'},
+//   {id: 3, title: 'テスト', body: 'テスト'}
+// ]
+
+const events = (state = [], action) => {
+  switch (action.type) {
+    case "CREATE_EVENT":
+      const event = { title: action.title, body: action.body };
+      const length = state.length;
+      let id = length === 0 ? 1 : state[length - 1].id + 1;
+
+      // {id: id}はショートハンドで{id}とかける
+      return [...state, { id, ...event }];
+    case " DELETE_EVENT":
+      return state;
+    case " DELETE_ALL_EVENT":
+      return [];
+    default:
+      return state;
+  }
+};
+
+export default evenets;
