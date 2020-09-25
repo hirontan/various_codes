bf44e40d6a294c668f1eeca5b7df7d8585d223de
hirontan <hirontan@sora.flights>
2020-09-25 21:22:06 +0900

    複数の状態を１つのオブジェクに統合

--- src/react/react-hooks/src/App.js
+++ src/react/react-hooks/src/App.js
@@ -16,24 +16,33 @@ const App = (props) => {
   //   setCount((previousCount) =>
   //     previousCount % 3 === 0 ? previousCount / 3 : previousCount
   //   );
+  const [state, setState] = useState(props);
+  const { name, price } = state;
+  // const [name, setName] = useState(props.name);
+  // const [price, setPrice] = useState(props.price);
 
-  const [name, setName] = useState(props.name);
-  const [price, setPrice] = useState(props.price);
-
-  const reset = () => {
-    setPrice(props.price);
-    setName(props.name);
-  };
+  // const reset = () => {
+  //   // setPrice(props.price);
+  //   // setName(props.name);
+  //   setState(props);
+  // };
 
   return (
     <>
       <p>
         現在の{name}は、{price}円です。
       </p>
-      <button onClick={() => setPrice(price + 1)}>+1</button>
-      <button onClick={() => setPrice(price - 1)}>-1</button>
-      <button onClick={reset}>Reset</button>
-      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <button onClick={() => setState({ ...state, price: price + 1 })}>
+        +1
+      </button>
+      <button onClick={() => setState({ ...state, price: price - 1 })}>
+        -1
+      </button>
+      <button onClick={() => setState(props)}>Reset</button>
+      <input
+        value={state.name}
+        onChange={(e) => setState({ ...state, name: e.target.value })}
+      />
 
       {/* <div>count: {count}</div>
       <div>
