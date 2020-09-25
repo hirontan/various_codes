f967e933c598c4fd299a0fa43487b092acfef408
hirontan <hirontan@sora.flights>
2020-09-25 09:47:10 +0900

    [React] 複数の状態を管理

--- src/react/react-hooks/src/App.js
+++ src/react/react-hooks/src/App.js
@@ -1,25 +1,41 @@
 import React, { useState } from "react";
 
-const App = () => {
+const App = (props) => {
   // 状態, 設定
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
-  const increment = () => setCount(count + 1);
-  const decrement = () => setCount(count - 1);
+  // const increment = () => setCount(count + 1);
+  // const decrement = () => setCount(count - 1);
 
-  const increment2 = () => setCount((previousCount) => previousCount + 1);
-  const decrement2 = () => setCount((previousCount) => previousCount - 1);
+  // const increment2 = () => setCount((previousCount) => previousCount + 1);
+  // const decrement2 = () => setCount((previousCount) => previousCount - 1);
 
-  const reset = () => setCount(0);
-  const double = () => setCount(count * 2);
-  const divide3 = () =>
-    setCount((previousCount) =>
-      previousCount % 3 === 0 ? previousCount / 3 : previousCount
-    );
+  // const reset = () => setCount(0);
+  // const double = () => setCount(count * 2);
+  // const divide3 = () =>
+  //   setCount((previousCount) =>
+  //     previousCount % 3 === 0 ? previousCount / 3 : previousCount
+  //   );
+
+  const [name, setName] = useState(props.name);
+  const [price, setPrice] = useState(props.price);
+
+  const reset = () => {
+    setPrice(props.price);
+    setName(props.name);
+  };
 
   return (
     <>
-      <div>count: {count}</div>
+      <p>
+        現在の{name}は、{price}円です。
+      </p>
+      <button onClick={() => setPrice(price + 1)}>+1</button>
+      <button onClick={() => setPrice(price - 1)}>-1</button>
+      <button onClick={reset}>Reset</button>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+
+      {/* <div>count: {count}</div>
       <div>
         <button onClick={increment}>+1</button>
         <button onClick={decrement}>-1</button>
@@ -32,9 +48,14 @@ const App = () => {
         <button onClick={reset}>Reset</button>
         <button onClick={double}>x2</button>
         <button onClick={divide3}>3の倍数の時だけ3で割る</button>
-      </div>
+      </div> */}
     </>
   );
+};
+
+App.defaultProps = {
+  name: "サンプル",
+  price: 1000,
 };
 
 export default App;
