f9ec06285ccbb76a2cd7834a635b7cfb4349e436
hirontan <hirontan@sora.flights>
2020-09-26 10:55:49 +0900

    [React] イベントを削除するときに操作ログを保存

--- src/react/react-hooks/src/components/Event.js
+++ src/react/react-hooks/src/components/Event.js
@@ -1,8 +1,10 @@
 import React, { useContext } from "react";
 
-import { DELETE_EVENT } from "../actions";
+import { ADD_OPERATION_LOG, DELETE_EVENT } from "../actions";
 
 import AppContext from "../contexts/AppContext";
+
+import { timeCurrentIso8601 } from "../utils";
 
 const Event = ({ event }) => {
   const { dispatch } = useContext(AppContext);
@@ -11,7 +13,14 @@ const Event = ({ event }) => {
     const result = window.confirm(
       `イベント(id=${id})を本当に削除しても良いですか？`
     );
-    if (result) dispatch({ type: DELETE_EVENT, id });
+    if (result) {
+      dispatch({ type: DELETE_EVENT, id });
+      dispatch({
+        type: ADD_OPERATION_LOG,
+        description: `イベント(id=${id})を削除しました`,
+        operatedAt: timeCurrentIso8601(),
+      });
+    }
   };
   return (
     <tr>
