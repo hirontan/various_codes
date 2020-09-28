7d6dce87ae56069853bb454d802bf02dc5a541b8
hirontan <hirontan@sora.flights>
2020-09-28 10:47:48 +0900

    [TypeScript] データ型の復習

---
+++ src/react/learn-typescript/src/App.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+// constは再代入できない
+const name = "hello";
+
+// letは再代入できる
+let nameChange = "hello";
+nameChange = "hello2";
+
+// データの型を明示的に（アノテーション）
+let username: string = "Hello";
+let dummyNum: number = 2;
+let bool: boolean = true;
+let array1: boolean[] = [true, true, false];
+let array2: (string | number)[] = ["hello", 1, 0];
+
+interface NAME {
+  first: string;
+  last?: string | null; // ?をつけると、存在しなくても良い
+}
+
+let nameObj: NAME = { first: "Yamada", last: null };
+
+const func1 = (x: number, y: number): number => {
+  return x + y;
+};
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
