199d6ba67f07eba1679d47935fe497a44e9f99cd
hirontan <hirontan@sora.flights>
2020-09-29 21:32:10 +0900

    [TypeScript] Event Handler: データ型

--- src/react/learn-typescript/src/TestComponent.tsx
+++ src/react/learn-typescript/src/TestComponent.tsx
@@ -12,10 +12,17 @@ interface UserData {
 const TestComponent: React.FC<Props> = (props) => {
   const [count, setCount] = useState<number | null>(0);
   const [user, setUser] = useState<UserData>({ id: 1, name: "dummy" });
+  const [inputData, setInputData] = useState("");
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setInputData(e.target.value);
+
   return (
     <div>
       <h1>{props.text}</h1>
       <h1>{count}</h1>
+      <input type="text" value={inputData} onChange={handleInputChange} />
+      <h1>{inputData}</h1>
     </div>
   );
 };
