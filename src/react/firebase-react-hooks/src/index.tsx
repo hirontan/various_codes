cc67f69c94b554dc1f692ffa8df3477864e843d6
hirontan <hirontan@sora.flights>
2020-10-02 08:30:17 +0900

    [React] React-Router-DOM

--- src/react/firebase-react-hooks/src/index.tsx
+++ src/react/firebase-react-hooks/src/index.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+import { Route, BrowserRouter } from "react-router-dom";
+import Login from "./Login";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <>
+        <Route exact path="/" component={App} />
+        <Route exact path="/login" component={Login} />
+      </>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
