3773e5f95a496ffbf53c770c04409df10cab0b1f
hirontan <hirontan@sora.flights>
2020-10-03 09:37:36 +0900

    [React] Firebase認証機能

--- src/react/firebase-react-hooks/src/Login.tsx
+++ src/react/firebase-react-hooks/src/Login.tsx
@@ -1,7 +1,80 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import styles from "./Login.module.css";
+import { Button, FormControl, TextField, Typography } from "@material-ui/core";
+import { auth } from "./firebase";
 
-function Login() {
-  return <div>Login Component</div>;
-}
+const Login: React.FC = (props: any) => {
+  const [isLogin, setIsLogin] = useState(true);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      user && props.history.push("/");
+    });
+  }, [props.history]);
+
+  return (
+    <div className={styles.login__root}>
+      <h1>{isLogin ? "Login" : "Register"}</h1>
+      <br />
+      <FormControl>
+        <TextField
+          InputLabelProps={{ shrink: true }}
+          name="email"
+          label="E-mail"
+          value={email}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            setEmail(e.target.value);
+          }}
+        />
+        <br />
+        <TextField
+          InputLabelProps={{ shrink: true }}
+          name="password"
+          label="Password"
+          type="password"
+          value={password}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            setEmail(e.target.value);
+          }}
+        />
+      </FormControl>
+      <br />
+      <Button
+        variant="contained"
+        color="primary"
+        size="small"
+        onClick={
+          isLogin
+            ? async () => {
+                try {
+                  await auth.signInWithEmailAndPassword(email, password);
+                  props.history.push("/");
+                } catch (error) {
+                  alert(error.message);
+                }
+              }
+            : async () => {
+                try {
+                  await auth.createUserWithEmailAndPassword(email, password);
+                  props.history.push("/");
+                } catch (error) {
+                  alert(error.message);
+                }
+              }
+        }
+      >
+        {isLogin ? "Login" : "Registers"}
+      </Button>
+      <br />
+      <Typography align="center">
+        <span onClick={() => setIsLogin(!isLogin)}>
+          {isLogin ? "Create new account ?" : "Back to login"}
+        </span>
+      </Typography>
+    </div>
+  );
+};
 
 export default Login;
