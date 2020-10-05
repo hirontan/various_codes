d16bd354ec386cf782d4d04bcd103c7c0b82bcab
hirontan <hirontan@sora.flights>
2020-10-05 17:37:20 +0900

    [React] Propsの利用

--- src/react/myapp/src/App.tsx
+++ src/react/myapp/src/App.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 
+import TodoList from "./components/TodoList";
+
 // function App() {
 //   return <div className="App"></div>;
 // }
 
 const App: React.FC = () => {
-  return <div className="App"></div>;
+  const todos = [{ id: "t1", text: "コースの完了" }];
+
+  return (
+    <div className="App">
+      <TodoList items={todos} />
+    </div>
+  );
 };
 
 export default App;
