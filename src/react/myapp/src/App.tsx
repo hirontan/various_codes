e8ce2141edab098fd4a1176e21c7ba377f3f8120
hirontan <hirontan@sora.flights>
2020-10-05 17:28:25 +0900

    [React] ReactとTypeScriptを一緒に使う

--- src/react/myapp/src/App.tsx
+++ src/react/myapp/src/App.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
-function App() {
+// function App() {
+//   return <div className="App"></div>;
+// }
+
+const App: React.FC = () => {
   return <div className="App"></div>;
-}
+};
 
 export default App;
