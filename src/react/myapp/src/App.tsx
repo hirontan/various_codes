631dbc3bb6d2ddc9bf9f005c55cd35d134d8ac68
hirontan <hirontan@sora.flights>
2020-10-07 21:35:42 +0900

    [React] Stateと型

--- src/react/myapp/src/App.tsx
+++ src/react/myapp/src/App.tsx
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import NewTodo from "./components/NewTodo";
 
 import TodoList from "./components/TodoList";
+import { Todo } from "./todo.model";
 
 // function App() {
 //   return <div className="App"></div>;
 // }
 
 const App: React.FC = () => {
-  const todos = [{ id: "t1", text: "コースの完了" }];
+  const [todos, setTodos] = useState<Todo[]>([]);
   const todoAddHandler = (text: string) => {
     console.log(text);
+    setTodos([{ id: Math.random().toString(), text: text }]);
   };
 
   return (
