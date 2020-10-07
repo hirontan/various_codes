8113c73b5e7b40a269821554df7523e27adfdb21
hirontan <hirontan@sora.flights>
2020-10-07 20:16:57 +0900

    [React] コンポーネント間の連携

--- src/react/myapp/src/App.tsx
+++ src/react/myapp/src/App.tsx
@@ -9,10 +9,13 @@ import TodoList from "./components/TodoList";
 
 const App: React.FC = () => {
   const todos = [{ id: "t1", text: "コースの完了" }];
+  const todoAddHandler = (text: string) => {
+    console.log(text);
+  };
 
   return (
     <div className="App">
-      <NewTodo />
+      <NewTodo onAddTodo={todoAddHandler} />
       <TodoList items={todos} />
     </div>
   );
--- src/react/myapp/src/components/NewTodo.tsx
+++ src/react/myapp/src/components/NewTodo.tsx
@@ -1,12 +1,17 @@
 import React, { useRef } from "react";
 
-const NewTodo: React.FC = () => {
+type NewTodoProps = {
+  onAddTodo: (todoText: string) => void;
+};
+
+const NewTodo: React.FC<NewTodoProps> = (props) => {
   // DOM要素にアクセス
   const textInputRef = useRef<HTMLInputElement>(null);
   const todoSunmitHandler = (event: React.FormEvent) => {
     event.preventDefault();
     const enteredText = textInputRef.current!.value;
     console.log(enteredText);
+    props.onAddTodo(enteredText);
   };
 
   return (
