b3599616011fe0c41269a61fc6e9960eeec836ac
hirontan <hirontan@sora.flights>
2020-10-09 10:13:07 +0900

    [TypeScript] 型を使う！

--- src/typescript/base/app.js
+++ src/typescript/base/app.js
@@ -1,1 +1,8 @@
 console.log("コード作成!");
+function add(n1, n2) {
+    return n1 + n2;
+}
+var number1 = 5;
+var number2 = 2.8;
+var result = add(number1, number2);
+console.log(result);
--- src/typescript/base/app.ts
+++ src/typescript/base/app.ts
@@ -1,1 +1,11 @@
 console.log("コード作成!");
+
+function add(n1: number, n2: number) {
+  return n1 + n2;
+}
+
+const number1 = 5;
+const number2 = 2.8;
+
+const result = add(number1, number2);
+console.log(result);
