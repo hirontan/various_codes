99ee7aa1f45352c0c4710313852b890be1d1f4fd
hirontan <hirontan@sora.flights>
2020-10-11 00:34:35 +0900

    [TypeScript] Union型

--- src/typescript/base/app.js
+++ src/typescript/base/app.js
@@ -1,47 +1,14 @@
-// const person: {
-//   name: string;
-//   age: number;
-// } = {
-//   name: "test",
-//   age: 30,
-// };
-// const person: {
-//   name: string;
-//   age: number;
-//   hobbies: string[];
-//   role: [number, string, boolean];
-// } = {
-//   name: "test",
-//   age: 30,
-//   hobbies: ["Sports", "Cooking"],
-//   role: [2, "author", true],
-// };
-// const ADMIN = 0;
-// const READ_ONLY = 1;
-// const AUTHOR = 2;
-var Role;
-(function (Role) {
-    Role[Role["ADMIN"] = 0] = "ADMIN";
-    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
-    Role[Role["AUTHOR"] = 2] = "AUTHOR";
-})(Role || (Role = {}));
-var person = {
-    name: "test",
-    age: 30,
-    hobbies: ["Sports", "Cooking"],
-    role: Role.ADMIN
-};
-// person.role.push("admin");
-// person.role[1] = 10;
-// person.role = [0, "admin", "user"];
-var favoriteActivities;
-favoriteActivities = ["Sports", 5, true];
-console.log(person.name);
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby);
-    console.log(hobby.toUpperCase());
+function combine(input1, input2) {
+    var result;
+    if (typeof input1 === "number" && typeof input2 === "number") {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
 }
-if (person.role === Role.ADMIN) {
-    console.log("管理者ユーザ");
-}
+var combineAges = combine(30, 26);
+console.log(combineAges);
+var combinedNames = combine("test1", "test2");
+console.log(combinedNames);
