1a6daf2b926e8fbd1a3bfa4965f7bfbef82286b1
hirontan <hirontan@sora.flights>
2020-10-11 00:14:15 +0900

    [TypeScript] Array型

--- src/typescript/base/app.js
+++ src/typescript/base/app.js
@@ -7,6 +7,14 @@
 // };
 var person = {
     name: "test",
-    age: 30
+    age: 30,
+    hobbies: ["Sports", "Cooking"]
 };
+var favoriteActivities;
+favoriteActivities = ["Sports"];
 console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby);
+    console.log(hobby.toUpperCase());
+}
--- src/typescript/base/app.ts
+++ src/typescript/base/app.ts
@@ -9,6 +9,15 @@
 const person = {
   name: "test",
   age: 30,
+  hobbies: ["Sports", "Cooking"],
 };
 
+let favoriteActivities: string[];
+favoriteActivities = ["Sports"];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby);
+  console.log(hobby.toUpperCase());
+}
