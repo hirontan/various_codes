d7f0e1f17d51df732bda307a89e1e26c26b37270
hirontan <hirontan@sora.flights>
2020-10-10 16:32:43 +0900

    [TypeScript] number, string, booleanの型

--- src/typescript/base/app.js
+++ src/typescript/base/app.js
@@ -1,12 +1,19 @@
 console.log("コード作成!");
-function add(n1, n2) {
+function add(n1, n2, showResult, phrase) {
     console.log(typeof n1);
     // if (typeof n1 !== "number" || typeof n2 !== "number") {
     //   throw new Error("入力値が正しくありません");
     // }
-    return n1 + n2;
+    if (showResult) {
+        console.log(phrase + (n1 + n2));
+    }
+    else {
+        return n1 + n2;
+    }
 }
 var number1 = 5;
 var number2 = 2.8;
-var result = add(number1, number2);
+var printResult = true;
+var resultPhrase = "Result: ";
+var result = add(number1, number2, printResult, resultPhrase);
 console.log(result);
