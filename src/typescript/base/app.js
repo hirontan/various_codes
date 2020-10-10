41b218d7b71834cf8038004e28ebba78d9424998
hirontan <hirontan@sora.flights>
2020-10-11 00:06:47 +0900

    [TypeScript] Object型

--- src/typescript/base/app.js
+++ src/typescript/base/app.js
@@ -1,19 +1,12 @@
-console.log("コード作成!");
-function add(n1, n2, showResult, phrase) {
-    console.log(typeof n1);
-    // if (typeof n1 !== "number" || typeof n2 !== "number") {
-    //   throw new Error("入力値が正しくありません");
-    // }
-    if (showResult) {
-        console.log(phrase + (n1 + n2));
-    }
-    else {
-        return n1 + n2;
-    }
-}
-var number1 = 5;
-var number2 = 2.8;
-var printResult = true;
-var resultPhrase = "Result: ";
-var result = add(number1, number2, printResult, resultPhrase);
-console.log(result);
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+//   name: "test",
+//   age: 30,
+// };
+var person = {
+    name: "test",
+    age: 30
+};
+console.log(person.name);
--- src/typescript/base/app.ts
+++ src/typescript/base/app.ts
@@ -1,23 +1,14 @@
-console.log("コード作成!");
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+//   name: "test",
+//   age: 30,
+// };
 
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-  console.log(typeof n1);
-  // if (typeof n1 !== "number" || typeof n2 !== "number") {
-  //   throw new Error("入力値が正しくありません");
-  // }
-  const result = n1 + n2;
-  if (showResult) {
-    console.log(phrase + result);
-  } else {
-    return n1 + n2;
-  }
-}
+const person = {
+  name: "test",
+  age: 30,
+};
 
-let number1: number;
-number1 = 5;
-const number2 = 2.8;
-const printResult = true;
-const resultPhrase = "Result: ";
-
-const result = add(number1, number2, printResult, resultPhrase);
-console.log(result);
+console.log(person.name);
