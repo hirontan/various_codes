2c00bbf70312ff8ec87061ba982ffe5df75cef12
hirontan <hirontan@sora.flights>
2020-10-11 00:22:35 +0900

    [TypeScript] 役割を作ってみる

--- src/typescript/base/app.js
+++ src/typescript/base/app.js
@@ -5,11 +5,29 @@
 //   name: "test",
 //   age: 30,
 // };
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string, boolean];
+// } = {
+//   name: "test",
+//   age: 30,
+//   hobbies: ["Sports", "Cooking"],
+//   role: [2, "author", true],
+// };
+var ADMIN = 0;
+var READ_ONLY = 1;
+var AUTHOR = 2;
 var person = {
     name: "test",
     age: 30,
-    hobbies: ["Sports", "Cooking"]
+    hobbies: ["Sports", "Cooking"],
+    role: ADMIN
 };
+// person.role.push("admin");
+// person.role[1] = 10;
+// person.role = [0, "admin", "user"];
 var favoriteActivities;
 favoriteActivities = ["Sports"];
 console.log(person.name);
@@ -17,4 +35,7 @@ for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby);
     console.log(hobby.toUpperCase());
+}
+if (person.role === ADMIN) {
+    console.log("管理者ユーザ");
 }
--- src/typescript/base/app.ts
+++ src/typescript/base/app.ts
@@ -6,22 +6,33 @@
 //   age: 30,
 // };
 
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string, boolean];
-} = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string, boolean];
+// } = {
+//   name: "test",
+//   age: 30,
+//   hobbies: ["Sports", "Cooking"],
+//   role: [2, "author", true],
+// };
+
+const ADMIN = 0;
+const READ_ONLY = 1;
+const AUTHOR = 2;
+
+const person = {
   name: "test",
   age: 30,
   hobbies: ["Sports", "Cooking"],
-  role: [2, "author", true],
+  role: ADMIN,
 };
 
 // person.role.push("admin");
 // person.role[1] = 10;
 
-person.role = [0, "admin", "user"];
+// person.role = [0, "admin", "user"];
 
 let favoriteActivities: string[];
 favoriteActivities = ["Sports"];
@@ -31,4 +42,8 @@ console.log(person.name);
 for (const hobby of person.hobbies) {
   console.log(hobby);
   console.log(hobby.toUpperCase());
+}
+
+if (person.role === ADMIN) {
+  console.log("管理者ユーザ");
 }
