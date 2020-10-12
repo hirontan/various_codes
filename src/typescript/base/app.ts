daca810fd47f6d9d2e1ca35116f14fc5ebd7eba3
hirontan <hirontan@sora.flights>
2020-10-12 22:29:48 +0900

    [TypeScript] エイリアス

--- src/typescript/base/app.ts
+++ src/typescript/base/app.ts
@@ -1,7 +1,10 @@
+type Combinable = number | string;
+type ConversionDescriptor = "as-number" | "as-text";
+
 function combine(
-  input1: number | string,
-  input2: number | string,
-  resultConversion: "as-number" | "as-text"
+  input1: Combinable,
+  input2: Combinable,
+  resultConversion: ConversionDescriptor
 ) {
   let result;
   if (
