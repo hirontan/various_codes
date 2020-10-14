42df46709c68cad5aaad48d35875b1bcbfca6b47
hirontan <hirontan@sora.flights>
2020-10-14 10:29:03 +0900

    [TypeScript] Lib設定

--- src/typescript/base/app.js
+++ src/typescript/base/app.js
@@ -1,3 +1,8 @@
-"use strict";
+var age;
+age = 30;
 var userName2 = 'Max';
 console.log(userName2);
+var button = document.querySelector('button');
+button.addEventListener('click', function () {
+    console.log('Clicked');
+});
--- src/typescript/base/app.ts
+++ src/typescript/base/app.ts
@@ -1,3 +1,11 @@
+let age: number
+age = 30
+
 const userName2 = 'Max'
 
 console.log(userName2)
+
+const button = document.querySelector('button')!
+button.addEventListener('click', () => {
+  console.log('Clicked')
+})
