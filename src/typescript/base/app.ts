e92ca2acf4aa2dd6554e89076735bda891c80827
hirontan <hirontan@sora.flights>
2020-10-15 18:57:55 +0900

    [TypeScript] noUnusedLocals / noUnusedParameters / noImplicitReturnsを追加

--- src/typescript/base/app.ts
+++ src/typescript/base/app.ts
@@ -1,3 +1,5 @@
+let appId = 'abc'
+
 let age: number
 age = 30
 
@@ -10,3 +12,19 @@ const button = document.querySelector('button')!
 button.addEventListener('click', () => {
   console.log('Clicked')
 })
+
+function clickHandler(message: string, age: string){
+  // let userName='Max'
+  console.log('Clicked: ' + message)
+}
+
+if (button) {
+  button.addEventListener('click', clickHandler.bind(null, "You're welcome!", 30))
+}
+
+function add(n1: number, n2:number){
+  if (n1 + n2 > 0) {
+    return n1 + n2
+  }
+  return;
+}
