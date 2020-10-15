4d04791e2e431d931a60442ef660589f6510e5d2
hirontan <hirontan@sora.flights>
2020-10-15 10:04:46 +0900

    [TypeScript] outDir設定

---
+++ src/typescript/base/dist/app.js
@@ -0,0 +1,9 @@
+"use strict";
+var age;
+age = 30;
+var userName2 = 'Max';
+console.log(userName2);
+var button = document.querySelector('button');
+button.addEventListener('click', function () {
+    console.log('Clicked');
+});
