25ce2b4a6494cc09e4a8bf54d700ff9685383edc
hirontan <hirontan@sora.flights>
2020-10-18 00:11:34 +0900

    [TypeScript] コンストラクタ関数とthis

--- src/typescript/base/app.ts
+++ src/typescript/base/app.ts
@@ -1,11 +1,23 @@
 class Department {
   name: string;
 
+  // 初期化用
   constructor(n: string) {
     this.name = n;
+  }
+
+  describe(this: Department) {
+    console.log("Department: " + this.name);
   }
 }
 
 const accounting = new Department("Accounting");
 
 console.log(accounting);
+
+accounting.describe();
+
+const accountingCopy = { name: "DUMMY", describe: accounting.describe };
+
+// 上記でダミーとしてオブジェクトが作られたので、undefinedになる
+accountingCopy.describe();
--- src/typescript/base/dist/app.js
+++ src/typescript/base/dist/app.js
@@ -3,7 +3,13 @@ var Department = (function () {
     function Department(n) {
         this.name = n;
     }
+    Department.prototype.describe = function () {
+        console.log("Department: " + this.name);
+    };
     return Department;
 }());
 var accounting = new Department("Accounting");
 console.log(accounting);
+accounting.describe();
+var accountingCopy = { name: "DUMMY", describe: accounting.describe };
+accountingCopy.describe();
