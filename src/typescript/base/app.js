2133fe3c727eabb3878d521c24e4d38688ad6b60
hirontan <hirontan@sora.flights>
2020-10-17 11:44:31 +0900

    [TypeScript] アロー関数

--- src/typescript/base/app.js
+++ src/typescript/base/app.js
@@ -1,8 +1,38 @@
-var age;
-age = 30;
-var userName2 = 'Max';
-console.log(userName2);
-var button = document.querySelector('button');
-button.addEventListener('click', function () {
-    console.log('Clicked');
-});
+// constは値を変更できない
+var userName = "Max";
+// userName = 'test'
+// letは値を変更できる
+var age = 30;
+age = 29;
+// function add(a: number, b: number){
+//   let result
+//   result = a + b
+//   return result
+// }
+// const add = (a: number, b: number) => {
+//   return a + b;
+// };
+// 一つの式だけを利用する場合は、{}とreturnを省略できる
+var add = function (a, b) { return a + b; };
+var printOutput = function (output) {
+    console.log(output);
+};
+printOutput(add(2, 5));
+var button = document.querySelector("button");
+if (button) {
+    button.addEventListener("click", function (event) {
+        console.log(event);
+    });
+}
+// var はグローバルスコープか、関数スコープのみしかない
+if (age >= 20) {
+    var isAdult = true;
+}
+// isAdultが活きている
+console.log(isAdult);
+// let はブロックスコープ
+if (age >= 20) {
+    var isAdult_let = true;
+}
+// isAdultが活きていない
+// console.log (isAdult_let)
--- src/typescript/base/app.ts
+++ src/typescript/base/app.ts
@@ -1,29 +1,50 @@
 // constは値を変更できない
-const userName = 'Max'
+const userName = "Max";
 // userName = 'test'
 
 // letは値を変更できる
-let age = 30
+let age = 30;
 
-age = 29
+age = 29;
 
-function add(a: number, b: number){
-  let result
-  result = a + b
-  return result
+// function add(a: number, b: number){
+//   let result
+//   result = a + b
+//   return result
+// }
+
+// const add = (a: number, b: number) => {
+//   return a + b;
+// };
+
+// 一つの式だけを利用する場合は、{}とreturnを省略できる
+const add = (a: number, b: number) => a + b;
+
+const printOutput: (output: string | number) => void = (output) => {
+  console.log(output);
+};
+
+printOutput(add(2, 5));
+
+const button = document.querySelector("button");
+
+if (button) {
+  button.addEventListener("click", (event) => {
+    console.log(event);
+  });
 }
 
 // var はグローバルスコープか、関数スコープのみしかない
-if (age >= 20){
-  var isAdult = true
+if (age >= 20) {
+  var isAdult = true;
 }
 
 // isAdultが活きている
-console.log (isAdult)
+console.log(isAdult);
 
 // let はブロックスコープ
-if (age >= 20){
-  let isAdult_let = true
+if (age >= 20) {
+  let isAdult_let = true;
 }
 
 // isAdultが活きていない
