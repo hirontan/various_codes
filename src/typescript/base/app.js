d39437c6125b0d0954c807c3377309a9c961d35c
hirontan <hirontan@sora.flights>
2020-10-26 01:08:54 +0900

    [TypeScript] クラスのインターフェース実装

--- src/typescript/base/app.js
+++ src/typescript/base/app.js
@@ -1,9 +1,26 @@
-var user1;
-user1 = {
-    name: "Max",
-    age: 3,
-    greet: function (phrase) {
-        console.log(phrase + " " + this.name);
+// オブジェクトの定義
+// interface Person {
+//   name: string;
+//   age: number;
+var Person = /** @class */ (function () {
+    function Person(n) {
+        this.age = 30;
+        this.name = n;
     }
-};
+    Person.prototype.greet = function (phrase) {
+        console.log(phrase + " " + this.name);
+    };
+    return Person;
+}());
+// let user1: Person;
+var user1;
+// user1 = {
+//   name: "Max",
+//   age: 3,
+//   greet(phrase: string) {
+//     console.log(phrase + " " + this.name);
+//   },
+// };
+user1 = new Person("Max");
 user1.greet("Hello I am");
+console.log(user1);
--- src/typescript/base/app.ts
+++ src/typescript/base/app.ts
@@ -1,19 +1,51 @@
 // オブジェクトの定義
-interface Person {
-  name: string;
-  age: number;
+// interface Person {
+//   name: string;
+//   age: number;
 
+//   greet(phrase: string): void;
+// }
+
+// type キーワードを使って、カスタム型にエイリアス名を付けられる
+// interfaceはオブジェクトのみで扱えたが、typeはユニオンなど様々な型を定義することができる
+// type Person = {
+//   name: string;
+//   age: number;
+
+//   greet(phrase: string): void;
+// };
+
+interface Greetable {
+  name: string;
   greet(phrase: string): void;
 }
 
-let user1: Person;
+class Person implements Greetable {
+  name: string;
+  age = 30;
 
-user1 = {
-  name: "Max",
-  age: 3,
+  constructor(n: string) {
+    this.name = n;
+  }
+
   greet(phrase: string) {
     console.log(phrase + " " + this.name);
-  },
-};
+  }
+}
+
+// let user1: Person;
+
+let user1: Greetable;
+
+// user1 = {
+//   name: "Max",
+//   age: 3,
+//   greet(phrase: string) {
+//     console.log(phrase + " " + this.name);
+//   },
+// };
+
+user1 = new Person("Max");
 
 user1.greet("Hello I am");
+console.log(user1);
