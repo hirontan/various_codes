181c5e0a1273ea4c1dccb950951674e02544a510
hirontan <hirontan@sora.flights>
2021-09-08 10:00:08 +0900

    [Go: Update] :=を使ってvarや型を宣言せずに初期化できる

--- src/go/variables/variables.go
+++ src/go/variables/variables.go
@@ -4,10 +4,13 @@ import "fmt"
 
 func main() {
 	// int 数字を扱う型で変数名iを宣言する
-	var i int
+	var i int = 30
+	// :=を使ってvarや型を宣言せずに初期化できる
+	j := 5
 	// iという変数の中に10という値を代入する
-	i = 10
+	// i = 10
 	// 値を書き換える
-	i = 20
-	fmt.Println(i)
+	// i = 20
+
+	fmt.Println(i, j)
 }
