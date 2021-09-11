f1756b0effad08893f5e0ad7296a694c88d7b0c7
hirontan <hirontan@sora.flights>
2021-09-11 21:23:57 +0900

    [Go: Update] 平方根をループで処理

--- src/go/loop/loop.go
+++ src/go/loop/loop.go
@@ -1,9 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+func Sqrt(x float64) float64 {
+	z := 1.0
+	for math.Abs((z*z)-x) > 0.1 {
+		z -= (z*z - x) / (2 * z)
+	}
+	return z
+}
 
 func main() {
-	for {
-		fmt.Println("Loop!")
-	}
+	fmt.Println(Sqrt(2))
 }
