92c92b7bd2fe1af6e777c13c6edd573db8c2d898
hirontan <hirontan@sora.flights>
2021-09-14 17:42:18 +0900

    [Go: Update] HelloWorldの動きについて、コメント

--- src/go/web_hello_world/main.go
+++ src/go/web_hello_world/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"net/http"
+	"net/http" // HTTPプロトコルを利用してくれるパッケージ
 )
 
 func handler(writer http.ResponseWriter, request *http.Request) {
@@ -10,6 +10,10 @@ func handler(writer http.ResponseWriter, request *http.Request) {
 }
 
 func main() {
+	// パスを指定したときに動作をハンドリングする
 	http.HandleFunc("/", handler)
+
+	// サーバを自分のPCの中で起動
+	// ポート: 8080
 	http.ListenAndServe(":8080", nil)
 }
