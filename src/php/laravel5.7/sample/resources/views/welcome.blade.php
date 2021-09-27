e7f7d7e20ad56f120167954a51e203410e7affe4
hirontan <hirontan@sora.flights>
2021-09-27 22:28:33 +0900

    [Add: PHP] トップページをHello Laravelに変更してみる

--- src/php/laravel5.7/sample/resources/views/welcome.blade.php
+++ src/php/laravel5.7/sample/resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Hello Laravel
                 </div>
 
                 <div class="links">
