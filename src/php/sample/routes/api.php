10280d21df8ad10662a95dcd11399e296b753fae
hirontan <hirontan@sora.flights>
2022-05-03 13:34:05 +0900

    [Add] サンプルAPI作成

--- src/php/sample/routes/api.php
+++ src/php/sample/routes/api.php
@@ -17,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('test', static function () {
+    return array('sample test');
+});
+
+Route::post('test', static function(Request $request) {
+    $request->validate(['text' => 'required|string']);
+    return \App\Models\Sample::create(['text' => $request->input('text')]);
+});
