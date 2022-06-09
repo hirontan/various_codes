b765ef4211425e6b0c5303fc4cf63ccf18a4a393
hirontan <hirontan@harmoniainc.jp>
2022-06-09 23:01:33 +0900

    [Add] テストユーザ発行用

---
+++ src/php/sample/database/seeders/UsersTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $param = [
+            'name' => 'user',
+            'email' => 'user@example.com',
+            'password' => Hash::make('password')
+        ];
+
+        DB::table('users') -> insert($param);
+    }
+}
