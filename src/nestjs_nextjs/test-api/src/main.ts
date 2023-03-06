7ca99e6618e401fe8d6ee0082fee431134aa26c8
hirontan <hirontan@harmoniainc.jp>
2023-03-07 00:07:12 +0900

    chore: Corsなどの設定

--- src/nestjs_nextjs/test-api/src/main.ts
+++ src/nestjs_nextjs/test-api/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common'; // Classバリデーション
+import { Request } from 'express';
+import * as cookieParser from 'cookie-parser'; // Cookie取り出す
+import * as csurf from 'csurf'; // csrfトークンを使う
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.enableCors({
+    credentials: true,
+    origin: ['http://localhost:3000'],
+  });
+  app.use(cookieParser());
   await app.listen(3300);
 }
 bootstrap();
