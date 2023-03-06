cff98ed16c9a5f56ba8d5697089b336639580b5b
hirontan <hirontan@harmoniainc.jp>
2023-03-06 23:54:09 +0900

    chore: サービス・コントローラ・モジュールの生成

--- src/nestjs_nextjs/test-api/src/app.module.ts
+++ src/nestjs_nextjs/test-api/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { TodoModule } from './todo/todo.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, TodoModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
