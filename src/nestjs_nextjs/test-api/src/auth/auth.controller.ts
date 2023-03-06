c4bee2076e1bb377430a07ef8231ff2b8797283b
hirontan <hirontan@harmoniainc.jp>
2023-03-07 00:45:27 +0900

    feat: サインアップAPI

--- src/nestjs_nextjs/test-api/src/auth/auth.controller.ts
+++ src/nestjs_nextjs/test-api/src/auth/auth.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  HttpCode,
+  HttpStatus,
+  Res,
+  Req,
+  Get,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+import { AuthService } from './auth.service';
+import { AuthDto } from './dto/auth.dto';
+import { Msg } from './interfaces/auth.interface';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('signup')
+  signUp(@Body() dto: AuthDto): Promise<Msg> {
+    return this.authService.signUp(dto);
+  }
+}
