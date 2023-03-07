113f044de3cdaba2f87cf50bd11e3eaf6ffbe6eb
hirontan <hirontan@harmoniainc.jp>
2023-03-07 09:15:56 +0900

    feat: JWT認証を追加し、ユーザ取得・nickname更新

--- src/nestjs_nextjs/test-api/src/user/user.service.ts
+++ src/nestjs_nextjs/test-api/src/user/user.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { PrismaService } from '../prisma/prisma.service';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  async updateUser(
+    userId: number,
+    dto: UpdateUserDto,
+  ): Promise<Omit<User, 'hashedPassword'>> {
+    const user = await this.prisma.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        ...dto,
+      },
+    });
+    delete user.hashedPassword;
+    return user;
+  }
+}
