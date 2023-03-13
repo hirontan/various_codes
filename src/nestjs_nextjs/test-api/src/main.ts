cd8fd0da0f297dc4b21879f9e4669d768099f510
hirontan <hirontan@harmoniainc.jp>
2023-03-13 23:28:42 +0900

    feat: CSRFトークンを利用する

--- src/nestjs_nextjs/test-api/src/main.ts
+++ src/nestjs_nextjs/test-api/src/main.ts
@@ -13,6 +13,18 @@ async function bootstrap() {
     origin: ['http://localhost:3000'],
   });
   app.use(cookieParser());
+  app.use(
+    csurf({
+      cookie: {
+        httpOnly: true,
+        sameSite: 'none',
+        secure: false,
+      },
+      value: (req: Request) => {
+        return req.header('csrf-token');
+      },
+    }),
+  );
   await app.listen(process.env.PORT || 3300);
 }
 bootstrap();
