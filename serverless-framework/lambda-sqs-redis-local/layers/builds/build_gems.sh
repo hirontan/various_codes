afc7844c2c2a835d2b181b00c11497ce7d4cb1df
hirontan <hirontan@harmoniainc.jp>
2024-04-28 10:52:44 +0900

    chore: amazon/aws-lambda-ruby:3.3ベースに構築する

--- serverless-framework/lambda-sqs-redis-local/layers/builds/build_gems.sh
+++ serverless-framework/lambda-sqs-redis-local/layers/builds/build_gems.sh
@@ -6,12 +6,12 @@ LAYER=sample_gems
 
 rm -rf ${LAYER} && mkdir -p ${LAYER}/ruby/gems
 
-docker build -t ruby32-builder -f layers/docker/Dockerfile .
+docker build -t ruby33-builder -f layers/docker/Dockerfile .
 
-CONTAINER=$(docker run -d ruby32-builder false)
+CONTAINER=$(docker run -d ruby33-builder false)
 
 docker cp \
-    $CONTAINER:/var/task/vendor/bundle/ruby/3.2.0 \
-    ${LAYER}/ruby/gems/3.2.0
+    $CONTAINER:/var/task/vendor/bundle/ruby/3.3.0 \
+    ${LAYER}/ruby/gems/3.3.0
 
 docker rm $CONTAINER --force
