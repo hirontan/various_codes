9d3fb354695e9dcc4d1c8ad3064ec3cd29e6f7bf
hirontan <hirontan@harmoniainc.jp>
2024-04-28 11:08:10 +0900

    chore: テスト用のキューを投入するスクリプト

---
+++ serverless-framework/lambda-sqs-redis-local/scripts/test_queue.rb
@@ -0,0 +1,14 @@
+#! /usr/bin/env ruby
+
+require 'aws-sdk-sqs'
+
+# SQSクライアントの作成
+sqs_client = Aws::SQS::Client.new(
+  region: 'ap-northeast-1',
+  endpoint: 'http://localhost:9324', # ローカルのエンドポイント
+  credentials: Aws::Credentials.new('dummy', 'dummy')
+)
+
+# SQSキューにメッセージを送信
+queue_url = 'http://localhost:9324/000000000000/my-queue-development' # ローカルのキューURL
+sqs_client.send_message(queue_url: queue_url, message_body: 'Test message')
