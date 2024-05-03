32da1fcd4bcc09941fada40db45f947765142e77
hirontan <hirontan@harmoniainc.jp>
2024-05-04 06:49:18 +0900

    chore: バインドポート変更に伴う修正

--- serverless-framework/lambda-sqs-redis-local/scripts/test_queue.rb
+++ serverless-framework/lambda-sqs-redis-local/scripts/test_queue.rb
@@ -5,10 +5,10 @@ require 'aws-sdk-sqs'
 # SQSクライアントの作成
 sqs_client = Aws::SQS::Client.new(
   region: 'ap-northeast-1',
-  endpoint: 'http://localhost:9324', # ローカルのエンドポイント
+  endpoint: 'http://localhost:59324', # ローカルのエンドポイント
   credentials: Aws::Credentials.new('dummy', 'dummy')
 )
 
 # SQSキューにメッセージを送信
-queue_url = 'http://localhost:9324/000000000000/my-queue-development' # ローカルのキューURL
+queue_url = 'http://localhost:59324/000000000000/my-queue-development' # ローカルのキューURL
 sqs_client.send_message(queue_url: queue_url, message_body: 'Test message')
